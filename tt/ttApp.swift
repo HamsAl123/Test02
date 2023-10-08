ca69a5d03d67d4cba28d9c9e3bf490d377a3e4a1
Bayan999 <128540841+Bayan999@users.noreply.github.com>
2023-10-08 12:28:23 +0300

    Add files via upload
    اخخ

---
+++ tt/ttApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttApp.swift
+//  tt
+//
+//  Created by Bayan on 22/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
