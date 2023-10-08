a5f6b5f11cee6158de1a37db75babd2483a19b45
Hams Alzahrani <hamsmohsen692@gmail.com>
2023-10-08 09:41:55 +0300

    Initial Commit

---
+++ Test02/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Test02
+//
+//  Created by Hams Alzahrani on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.brown)
+                .ignoresSafeArea()
+                
+            VStack{
+                Image("images").resizable().frame(width: 500,height: 300).clipShape(Circle()).overlay(Circle().stroke(Color.yellow, lineWidth: 5))
+               
+                Text("Hams Alzahrani")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.leading)
+                    .padding(.trailing, 150.0)
+                Text("Hi!! I'm Hams and I would say that I am a huge bookworm. I started reading when I was in middle school and since then I couldn't stop").foregroundColor(.black).padding(.horizontal, 60.0)
+            }
+                .padding()
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+   
---
+++ Test02/Test02App.swift
@@ -0,0 +1,17 @@
+//
+//  Test02App.swift
+//  Test02
+//
+//  Created by Hams Alzahrani on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Test02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
